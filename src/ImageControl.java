88f8e7d93becfe6bfcfe30d3a183079f531928be
mishashostak <114440497+mishashostak@users.noreply.github.com>
2023-01-17 18:24:34 -0500

    Add basic ImageControl logic and test image

--- src/ImageControl.java
+++ src/ImageControl.java
@@ -1,4 +1,7 @@
+import java.awt.Graphics2D;
+import java.awt.color.ColorSpace;
 import java.awt.image.BufferedImage;
+import java.awt.image.ColorConvertOp;
 import java.io.File;
 import java.io.IOException;
 
@@ -28,12 +31,26 @@ public class ImageControl {
         try {
             srcImg = ImageIO.read(file);
         } catch (IOException e) {e.printStackTrace();}
+        Graphics2D g = srcImg.createGraphics();
+        g.drawImage(srcImg, null, 0, 0);
+			BufferedImage grayImage = new BufferedImage(srcImg.getWidth(), srcImg.getHeight(), BufferedImage.TYPE_BYTE_GRAY);
+			g = grayImage.createGraphics();
+			g.drawImage(srcImg, 0, 0, null);
 
+            ColorConvertOp op = new ColorConvertOp(ColorSpace.getInstance(ColorSpace.CS_GRAY), null);
+            op.filter(grayImage,grayImage);
+
+            BufferedImage blackWhite = new BufferedImage(srcImg.getWidth(), srcImg.getHeight(), BufferedImage.TYPE_BYTE_BINARY);
+            g = blackWhite.createGraphics();
+            g.drawImage(grayImage, null, 0, 0);
+            g.dispose();
+        /*
         int[][] src2d = new int[srcImg.getHeight()][srcImg.getWidth()];
         for(int i = 0; i < 2048; i++)
             for(int j = 0; j < 2048; j++)
                 src2d[i][j] = srcImg.getRGB(i, j);
+        */
 
-        return null;
+        return blackWhite;
     }
 }
